a71034b076927e4292ccb695561056675ffddc36
Mamifero68 <143024516+lucas686768@users.noreply.github.com>
2024-09-17 16:19:30 -0300

    Add files via upload

---
+++ lucas faz muito bom/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'física',
+    'o que é física?',
+    'é a matematica que estuda a natureza'
+)
+
+criaCartao(
+    'matematica',
+    'a função 2x+4 é crescente?',
+    'sim'
+)
+
+criaCartao(
+    'química',
+    'qual a formula da água?',
+    'H2O'
+)
+
+criaCartao(
+    'Portugues',
+    'a frase "cara à cara" tem crase?',
+    'não'
+)
